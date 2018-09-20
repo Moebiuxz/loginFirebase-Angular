7aae07cb46672979b2f85020f53c4c16ef000e15
Álvaro Bunster <al.bunstr@gmail.com>
2018-09-20 16:46:15 -0300

    Servicio para registro de usuario
    Se implementó el método de registro de usuarios en firebase.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
-import { FirebaseAuthService} from 'firebase-angular2/core';
-import * as firebase from 'firebase';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor( public afAuth: FirebaseAuthService) { }
+  constructor( public afAuth: AngularFireAuth) { }
 
+  // Método para registrar usuario con email.
+  doRegister(usuario) {
+    return new Promise<any>((resolve, reject) => {
+      firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.password)
+        .then(res => {
+          resolve(res);
+        }, err => reject(err));
+    });
+  }
 }
